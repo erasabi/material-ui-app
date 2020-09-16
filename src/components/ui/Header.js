dd7d388d401390038983ac861a0cc381ada14909
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-09-16 02:01:22 -0400

    2.17 Theme part 3: Typography

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { Typography } from '@material-ui/core';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -27,7 +28,10 @@ function Header(props) {
         <ElevationScroll>
             <AppBar position="fixed">
                 <Toolbar>
-                    Arc Development
+                    <Typography variant="h3" >
+                        Arc Development
+                    </Typography>
+
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
